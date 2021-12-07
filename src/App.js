bdd8ab078eaa818208679347d2d7f37ba6066954
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-07 15:32:29 -0600

    notfound page implemented

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 // pages
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 // layout components
 import NavBar from './components/layout/NavBar';
 import Alert from './components/layout/Alert';
 // user components
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 // state components
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -30,18 +30,10 @@ const App = () => {
 						<div className='container'>
 							<Alert />
 							<Switch>
-								<Route
-									exact
-									path='/'
-									render={(props) => (
-										<>
-											<Search />
-											<Users />
-										</>
-									)}
-								/>
+								<Route exact path='/' component={Home} />
 								<Route exact path='/about' component={About} />
 								<Route exact path='/user/:login' component={User} />
+								<Route component={NotFound} />
 							</Switch>
 						</div>
 					</div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Users from '../users/Users';
+import Search from '../users/Search';
+
+const Home = () => {
+	return (
+		<>
+			<Search />
+			<Users />
+		</>
+	);
+};
+export default Home;
