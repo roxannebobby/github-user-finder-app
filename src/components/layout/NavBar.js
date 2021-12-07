66327adc9dc0d58e652af58632bf1ab459363060
Roxanne Weber <marie.sankofa@gmail.com>
2021-12-06 21:36:17 -0600

    links added to navbar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const NavBar = ({ icon, title }) => {
 	return (
-		<nav>
+		<nav className='navbar bg-primary'>
 			<h1>
 				<i className={icon} /> {title}
 			</h1>
+			<ul>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 };
